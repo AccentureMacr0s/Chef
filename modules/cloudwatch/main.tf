0a43b274bed8039e0f1f49ff375d0eeca497f2fb
mr.smaill213@gmail.com <mr.smaill213@gmail.com>
2024-01-20 16:38:02 +0200

    yes

---
+++ modules/cloudwatch/main.tf
@@ -0,0 +1,6 @@
+module "cloudwatch_log_group" {
+  source = "terraform-aws-modules/cloudwatch/aws//modules/log-group"
+
+  # ... настройки модуля, например, имя группы логов, политики удержания и т.д.
+}
+ 
